c3a9a6b35fde08100d0352004843159b4a675505
わだたける <68636852+wdtkr@users.noreply.github.com>
2023-08-23 12:54:48 +0900

    lib.bundleの生成

---
+++ lib.cpp
@@ -0,0 +1,23 @@
+#include <string>
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+void registerFqdn(std::string a)
+{
+}
+
+void sendBinaryData()
+{
+}
+
+void waitDataReceive()
+{
+}
+
+void cppCallBack()
+{
+    // テスト用。後々は、関数を引数に受け取ってから、その関数をコールバックする
+}
