b187e48a4d8784d17d109df7e82cd8108ddd6350
わだたける <68636852+wdtkr@users.noreply.github.com>
2023-09-02 03:03:23 +0900

    コールバック関数の呼び出し可能化

--- lib.cpp
+++ lib.cpp
@@ -1,10 +1,29 @@
 #include <string>
 #include <fstream>
+#include <cstring>      // for strcpy and strlen
+#include <cstdlib>      // for exit and EXIT_FAILURE
+#include <iostream>     // for cout (optional)
+#include <sys/socket.h> // for socket
+#include <netinet/in.h> // for sockaddr_in
+#include <unistd.h>     // for close
+#include <arpa/inet.h>  // for inet_addr
 #include "lib.h"
 
-int sum(int a, int b)
+std::thread *pThread = nullptr;
+CallbackType g_callback = nullptr;
+
+void setCallback(CallbackType callback)
 {
-    return a + b;
+    g_callback = callback;
+    triggerCallback();
+}
+
+void triggerCallback()
+{
+    if (g_callback != nullptr)
+    {
+        g_callback("Hello from C++");
+    }
 }
 
 const char *registerFqdn(const char *a)
@@ -13,6 +32,75 @@ const char *registerFqdn(const char *a)
     char *pszReturn = new char[strlen(szSampleString) + 1];
     strcpy(pszReturn, szSampleString);
     return pszReturn;
+}
+
+// UDPメッセージを送信する関数
+void sendUDPMessage(const char *IP, int port, const char *message)
+{
+    int sockfd;                  // ソケットのファイルディスクリプタを格納
+    struct sockaddr_in servaddr; // サーバーアドレス情報を格納
+
+    // UDPソケットの作成
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("socket creation failed"); // ソケット作成失敗時のエラーメッセージ
+        exit(EXIT_FAILURE);               // プログラムを終了
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr)); // servaddrを0で初期化
+
+    // サーバーのアドレス情報を設定
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(port);
+    servaddr.sin_addr.s_addr = inet_addr(IP);
+
+    // メッセージを送信
+    if (sendto(sockfd, message, strlen(message), 0, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        perror("send failed"); // 送信失敗時のエラーメッセージ
+        exit(EXIT_FAILURE);    // プログラムを終了
+    }
+
+    close(sockfd); // ソケットを閉じる
+}
+
+// UDPメッセージを受信する関数
+void receiveUDPMessage(int port)
+{
+    int sockfd;
+    struct sockaddr_in servaddr;
+
+    char buffer[1024];
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(port);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    while (true)
+    {
+        socklen_t len;
+        int n = recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr *)&servaddr, &len);
+        buffer[n] = '\0';
+
+        if (g_callback)
+        {
+            g_callback(buffer);
+        }
+    }
 }
 
 const char *sendBinaryData(const char *a)
--- lib.h
+++ lib.h
@@ -6,10 +6,15 @@
 #define UNITYEXPORT
 #endif
 
+typedef void (*CallbackType)(const char *);
+
 extern "C"
 {
-    UNITYEXPORT int UNITYCALLCONV sum(int a, int b);
+    UNITYEXPORT void UNITYCALLCONV setCallback(CallbackType callback);
+    UNITYEXPORT void UNITYCALLCONV triggerCallback();
     UNITYEXPORT const char UNITYCALLCONV *registerFqdn(const char *a);
+    UNITYEXPORT void UNITYCALLCONV sendUDPMessage(const char *IP, int port, const char *message);
+    UNITYEXPORT void UNITYCALLCONV receiveUDPMessage(int port);
     UNITYEXPORT const char UNITYCALLCONV *sendBinaryData(const char *a);
     void waitDataReceive();
     void cppCallBack();
