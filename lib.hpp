593a3aaf01cb1e3ac4e566cc633d87a38e3d5cf6
わだたける <68636852+wdtkr@users.noreply.github.com>
2023-09-29 01:51:25 +0900

    微修正

--- lib.hpp
+++ lib.hpp
@@ -11,7 +11,7 @@ typedef void (*CallbackType)(const char *);
 extern "C"
 {
     UNITYEXPORT void UNITYCALLCONV triggerCallback();
-    UNITYEXPORT void UNITYCALLCONV setCallback(CallbackType debug, CallbackType start, CallbackType receive);
+    UNITYEXPORT void UNITYCALLCONV setCallback(CallbackType debug, CallbackType receive, CallbackType start);
     UNITYEXPORT void UNITYCALLCONV sendUDPMessage(const char *IP, int port, const char *message);
     UNITYEXPORT void UNITYCALLCONV preReceiveUDPMessage(int port);
     UNITYEXPORT void UNITYCALLCONV receiveUDPMessage();
